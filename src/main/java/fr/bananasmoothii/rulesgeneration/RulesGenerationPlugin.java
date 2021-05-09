55f6c1305409e6c223b4f8f364eea407acb0d1d0
bananasmoothii <45853225+bananasmoothii@users.noreply.github.com>
2021-05-09 08:04:40 +0200

    Initial commit

---
+++ src/main/java/fr/bananasmoothii/rulesgeneration/RulesGenerationPlugin.java
@@ -0,0 +1,17 @@
+package fr.bananasmoothii.rulesgeneration;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RulesGenerationPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
